e411b84a488c1d9647b5c1cb868d33ee2b6e7a5c
davidsc2024 <2024_davids.carbajal@iticbcn.cat>
2024-11-03 17:00:38 +0100

    Delete ACTIVITAT3 directory

--- ACTIVITAT3/exercici1.py
+++
@@ -1,8 +0,0 @@
-#Ejercicio 1
-numeroUsuario = int(input("Introduce un numero del 10 al 100: "))
-
-# Creamos la tupla con los números de 1 hasta el número indicado por el usuario
-mi_tupla = tuple(range(1, numeroUsuario+1))
-
-# Imprimimos la tupla
-print(mi_tupla)
